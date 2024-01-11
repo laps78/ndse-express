744b9412fa43b904628bfd638ff9b9f94212b339
laps78 <9254384@mail.ru>
2024-01-11 16:42:04 +0300

    Прибрался в папке views. Продолжение разработке сервер-сайд интерфейса приложения.

--- routes/viewEngine.route.js
+++ routes/viewEngine.route.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (rew, res) => {
+router.get("/", (req, res) => {
   res.render("index", {
     title: 'Главная'
   })
