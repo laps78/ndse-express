52cb87174c8d99348c34e1c3e12842aa8d63804d
laps78 <sovtem@gmail.com>
2024-01-13 04:58:08 +0300

    реализованы главная страница и страница библиотеки.

--- routes/viewEngine.route.js
+++ routes/viewEngine.route.js
@@ -9,6 +9,14 @@ const books = storage.data;
 router.get("/", (req, res) => {
   res.render("index", {
     title: "Главная",
+    books: books,
+  });
+});
+
+router.get("/books", (req, res) => {
+  res.render("books/index", {
+    title: "Библиотека",
+    books: books,
   });
 });
 
@@ -18,9 +26,17 @@ router.get("/create", (req, res) => {
   });
 });
 
-router.post("/create", (req, res) => {
-  const { title, description } = req.body;
-  const newBook = new Book(title, description);
-  storage.write(newBook);
+router.get("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const idx = books.findIndex((el) => el.id === id);
+  if (idx === -1) {
+    res.redirect("/404");
+  }
+
+  res.render("books/update", {
+    title: "Библиотека | редактировать книгу",
+    book: books[idx],
+  });
 });
+
 module.exports = router;
