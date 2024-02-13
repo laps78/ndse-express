c428d6f333b4324a35b9f8a1cb9d9b7084d54f2a
laps78 <sovtem@gmail.com>
2024-02-13 11:50:57 +0300

    добавлен модуль работы с сервисом счетчика, также обновлены соответствующие роуты.

--- routes/viewEngine.route.js
+++ routes/viewEngine.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const counter = require("../src/counterApi");
 const formMulter = require("../src/middleware/uploadFromFormMW");
 
 const { Storage } = require("../src/storage.io");
@@ -12,10 +12,6 @@ router.use((req, res, next) => {
     req.storage = storage;
     next();
   }, 1000);
-});
-
-router.use((req, res, next) => {
-  next();
 });
 
 router.get("/", (req, res) => {
@@ -31,12 +27,15 @@ router.get("/books", (req, res) => {
   });
 });
 
-router.get("/books/:id", (req, res) => {
+router.get("/books/:id", async (req, res) => {
   const books = req.storage.data;
-  const idx = books.findIndex((elem) => elem.id === req.params.id);
+  const { id } = req.params;
+  const idx = books.findIndex((elem) => elem.id === id);
   if (idx !== -1) {
+    const counterResponce = await counter.fetch(`counter/${id}/incr`, "POST");
     res.render("books/view", {
       book: books[idx],
+      count: counterResponce.count,
     });
   } else {
     res.json("404 | книга не найдена");
@@ -100,10 +99,12 @@ router.post("/books/update/:id", formMulter, (req, res) => {
   }
 });
 
-router.post("/books/delete/:id", (req, res) => {
+router.post("/books/delete/:id", async (req, res) => {
   const books = req.storage.data;
-  const idx = books.findIndex((el) => el.id === req.params.id);
+  const { id } = req.params;
+  const idx = books.findIndex((el) => el.id === id);
   if (idx !== -1) {
+    await counter.fetch(`counter/${id}/delete`);
     books.splice(idx, 1);
     req.storage.write(books);
     res.status(200);
