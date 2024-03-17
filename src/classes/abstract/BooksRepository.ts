aadb1fbbec419cfeca415e3cffda363066a17fa1
laps78 <sovtem@gmail.com>
2024-03-17 12:53:16 +0300

    Линтер ts установлен

---
+++ src/classes/abstract/BooksRepository.ts
@@ -0,0 +1,26 @@
+abstract classs BooksRepository {
+  /**
+   * создание книги.
+   */
+  createBook(book){ }
+  
+  /**
+   * получение книги по id.
+   */
+  getBook(id){ }
+  
+  /**
+   * получение всех книг.
+   */
+  getBooks(){ }
+  
+  /**
+   * обновление книги.
+   */
+  updateBook(id){ }
+  
+  /**
+   * удаление книги.
+   */
+  deleteBook(id){}
+}
