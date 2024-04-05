61fe7660ba92a5564fb7dfd7e4b7c90236b0c8d7
laps78 <sovtem@gmail.com>
2024-04-05 11:30:01 +0300

    Связно имплементировал реализацию классов bookRepository и Bookservice. следующим этапом ожидается подключение этих классов к функционалу роутеров и дальнейшиф рефакторинг проект а в сторону ts

---
+++ src/classes/BookService.ts
@@ -0,0 +1,31 @@
+import { injectable } from "inversify";
+import { Book as BookType } from "../interfaces/Book";
+import bookModel from "../../models/book.model";
+import BooksRepository from "./abstract/BooksRepository";
+
+@injectable()
+export default class BookService extends BooksRepository {
+  async createBook(book: BookType) {
+    const newBook = new bookModel(book);
+    await newBook.save();
+    return newBook;
+  }
+
+  async getBooks(): Promise<BookType[] | null> {
+    const books: BookType[] = await bookModel.find().select("-__v");
+    return books;
+  }
+
+  async getBook(id: string): Promise<BookType | null> {
+    const foundBook: BookType = await bookModel.findById(id).select("-__v");
+    return foundBook;
+  }
+
+  async updateBook(id: string, newBookData: BookType) {
+    await bookModel.findByIdAndUpdate(id, newBookData);
+  }
+
+  async deleteBook(id: string) {
+    await bookModel.findByIdAndDelete(id);
+  }
+}
--- src/classes/abstract/BooksRepository.ts
+++ src/classes/abstract/BooksRepository.ts
@@ -3,6 +3,6 @@ export default abstract class BooksRepository {
   abstract getBooks(): Promise<Book[] | null>;
   abstract getBook(id: string): Promise<Book | null>;
   abstract createBook(book: Book): void;
-  abstract updateBook(id: string, book: Book): Promise<Book | null>;
-  abstract deleteBook(id: string): Promise<Book | null>;
+  abstract updateBook(id: string, newBookData: Book): void;
+  abstract deleteBook(id: string): void;
 }
